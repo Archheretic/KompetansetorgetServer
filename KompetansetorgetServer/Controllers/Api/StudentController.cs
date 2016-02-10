679f4fdabeb174219c22ffefaadc99487911e5c3
Archheretic <setervang@hotmail.com>
2016-02-10 15:44:32 +0100

    Add REST Api to Student

---
+++ KompetansetorgetServer/Controllers/Api/StudentController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Web.Http;
+using KompetansetorgetServer.ContextDbs;
+using KompetansetorgetServer.Models;
+using System.Linq;
+using System.Web.Http.Description;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Net;
+
+namespace KompetansetorgetServer
+{
+	public class StudentController : ApiController
+	{
+		private KompetanseContext db;
+
+		public StudentController ()
+		{
+			this.db = new KompetanseContext();
+		}
+
+		public IQueryable<Student> GetStudents()
+		{
+			return db.Students;
+		}
+
+		// GET: api/Students/5
+		[ResponseType(typeof(Student))]
+		public IHttpActionResult GetStudent(string id)
+		{
+			Student student = db.Students.Find(id);
+			if (student == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(student);
+		}
+
+		// PUT: api/Students/5
+		[ResponseType(typeof(void))]
+		public IHttpActionResult PutStudent(string id, Student student)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			if (id != student.Username)
+			{
+				return BadRequest();
+			}
+
+			db.Entry(student).State = EntityState.Modified;
+
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!StudentExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return StatusCode(HttpStatusCode.NoContent);
+		}
+
+		private bool StudentExists(string id)
+		{
+			return db.Students.Count(e => e.Username == id) > 0;
+		}
+	}
+}
+/*
+private IRepository<Beer> _beerRepository;
+
+public BeersController()
+{
+	_beerRepository = WebApiApplication.BeerRepository;
+}
+
+public IEnumerable<Beer> Get()
+{
+	return _beerRepository.Items.ToArray();
+}
+
+public Beer Get(Guid id)
+{
+	Beer entity = _beerRepository.Get(id);
+	if (entity == null)
+	{
+		throw new HttpResponseException(HttpStatusCode.NotFound);
+	}
+	return entity;
+}
+
+public HttpResponseMessage Post([FromBody] Beer value)
+{
+	var result = _beerRepository.Add(value);
+	if (result == null)
+	{
+		// the entity with this key already exists
+		throw new HttpResponseException(HttpStatusCode.Conflict);
+	}
+	var response = Request.CreateResponse<Beer>(HttpStatusCode.Created, value);
+	string uri = Url.Link("DefaultApi", new { id = value.Id });
+	response.Headers.Location = new Uri(uri);
+	return response;
+}
+
+public HttpResponseMessage Put(Guid id, Beer value)
+{
+	value.Id = id;
+	var result = _beerRepository.Update(value);
+	if (result == null)
+	{
+		// entity does not exist
+		throw new HttpResponseException(HttpStatusCode.NotFound);
+	}
+	return Request.CreateResponse(HttpStatusCode.NoContent);
+}
+
+public HttpResponseMessage Delete(Guid id)
+{
+	var result = _beerRepository.Delete(id);
+	if (result == null)
+	{
+		throw new HttpResponseException(HttpStatusCode.NotFound);
+	}
+	return Request.CreateResponse(HttpStatusCode.NoContent);
+}
+}
+
+*/
--- KompetansetorgetServer/Global.asax.cs
+++ KompetansetorgetServer/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Http;
+
 
 namespace KompetansetorgetServer
 {
@@ -29,9 +31,29 @@ namespace KompetansetorgetServer
 
 		protected void Application_Start ()
 		{
+			// The line under is taken from a ASP.NET 4.52 MVC Web Api application
+			// GlobalConfiguration i not recognized.
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+			//WebApiConfig.Register(GlobalConfiguration.Configuration);
+
 			AreaRegistration.RegisterAllAreas ();
 			RegisterGlobalFilters (GlobalFilters.Filters);
 			RegisterRoutes (RouteTable.Routes);
+		}
+	}
+
+	// The line under is taken from a ASP.NET 4.52 MVC Web Api application
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			); 
 		}
 	}
 }
