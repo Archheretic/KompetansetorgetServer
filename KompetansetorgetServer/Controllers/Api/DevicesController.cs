e3e3deb1229df4f9c81ebf10d2c3418f05414f21
Archheretic <setervang@hotmail.com>
2016-03-30 19:03:23 +0200

    add Rest Api for Devices  /api/devices

---
+++ KompetansetorgetServer/Controllers/Api/DevicesController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using KompetansetorgetServer.Models;
+
+namespace KompetansetorgetServer.Controllers.Api
+{
+    public class DevicesController : ApiController
+    {
+        private KompetansetorgetServerContext db = new KompetansetorgetServerContext();
+
+        // GET: api/Devices
+        public IQueryable GetDevices()
+        {
+            //return db.Devices;
+            return db.Devices.Select(d => new
+            {
+                d.IdDevice,
+                d.DeviceType,
+                Student = d.Student.Username,
+                d.Token
+            });
+        }
+
+        // GET: api/Devices/5
+        [ResponseType(typeof(Device))]
+        public async Task<IHttpActionResult> GetDevice(string id)
+        {
+            Device device = await db.Devices.FindAsync(id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            return Ok( new {
+                device.IdDevice,
+                device.DeviceType,
+                Student = device.Student.Username,
+                device.Token
+            });
+        }
+
+        // PUT: api/Devices/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutDevice(string id, Device device)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != device.IdDevice)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(device).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DeviceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Devices
+        [ResponseType(typeof(Device))]
+        public async Task<IHttpActionResult> PostDevice(Device device)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Devices.Add(device);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DeviceExists(device.IdDevice))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = device.IdDevice }, device);
+        }
+
+        // DELETE: api/Devices/5
+        [ResponseType(typeof(Device))]
+        public async Task<IHttpActionResult> DeleteDevice(string id)
+        {
+            Device device = await db.Devices.FindAsync(id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            db.Devices.Remove(device);
+            await db.SaveChangesAsync();
+
+            return Ok(device);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DeviceExists(string id)
+        {
+            return db.Devices.Count(e => e.IdDevice == id) > 0;
+        }
+    }
+}
